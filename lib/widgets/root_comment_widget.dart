9573a79f0b2df5a2af30f3a1456d777d70ed3307
adarsh <adarsh@webcastle.in>
2025-05-15 11:07:48 +0530

    *no line if there are no replies

--- lib/widgets/root_comment_widget.dart
+++ lib/widgets/root_comment_widget.dart
@@ -5,18 +5,16 @@ import 'package:provider/provider.dart';
 class RootCommentWidget extends StatelessWidget {
   final PreferredSizeWidget avatar;
   final Widget content;
+  final bool hasReplies;
 
-  const RootCommentWidget(this.avatar, this.content);
+  const RootCommentWidget(this.avatar, this.content, {this.hasReplies = false});
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: RootPainter(
-        avatar.preferredSize,
-        context.watch<TreeThemeData>().lineColor,
-        context.watch<TreeThemeData>().lineWidth,
-          Directionality.of(context)
-      ),
+      painter: RootPainter(avatar.preferredSize, context.watch<TreeThemeData>().lineColor,
+          context.watch<TreeThemeData>().lineWidth, Directionality.of(context),
+          hasReplies: hasReplies),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -39,7 +37,9 @@ class RootPainter extends CustomPainter {
   Color? pathColor;
   double? strokeWidth;
   final TextDirection textDecoration;
-  RootPainter(this.avatar, this.pathColor, this.strokeWidth, this.textDecoration) {
+  final bool hasReplies;
+
+  RootPainter(this.avatar, this.pathColor, this.strokeWidth, this.textDecoration, {this.hasReplies = false}) {
     _paint = Paint()
       ..color = pathColor!
       ..style = PaintingStyle.stroke
@@ -49,11 +49,11 @@ class RootPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    if(textDecoration == TextDirection.rtl)
-    canvas.translate(size.width, 0);
+    if (!hasReplies) return;
+
+    if (textDecoration == TextDirection.rtl) canvas.translate(size.width, 0);
     double dx = avatar!.width / 2;
-    if(textDecoration == TextDirection.rtl)
-      dx *= -1;
+    if (textDecoration == TextDirection.rtl) dx *= -1;
     canvas.drawLine(
       Offset(dx, avatar!.height),
       Offset(dx, size.height),
